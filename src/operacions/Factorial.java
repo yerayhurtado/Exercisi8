9f2e8fc3b5c16f4b604e4117f4aa4dc387c7c94f
yerayhurtado <yerayhudra13@gmail.com>
2024-03-19 17:05:36 +0100

    FactorialRecursiu

--- src/operacions/Factorial.java
+++ src/operacions/Factorial.java
@@ -16,4 +16,14 @@ public class Factorial {
 		}
 		return aux;
 	}
+	public double factorialRecursiu(int n) {
+		if (n < 0) {
+		throw new IllegalArgumentException();
+		}
+		if (n == 1 || n == 0) {
+		return 1;
+		}
+		Factorial fmenos1 = new Factorial();
+		return n * fmenos1.factorialRecursiu(n - 1);
+		}
 }
